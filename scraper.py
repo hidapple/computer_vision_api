af539ae5413e041b77c94748a2faf158fe27b5d1
hidapple <shotfsh@gmail.com>
2017-05-29 23:17:27 +0900

    Add scraping script

---
+++ scraper.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "http://books.rakuten.co.jp/search/dt?mt=0&o=0&cy=0&h=100&g=001013001&e=0&v=2&spv=2&s=1&sv=30"
+html = urlopen(url)
+soup = BeautifulSoup(html, "html.parser")
+
+items = soup.find_all(class_='rbcomp__item-list__item__image')
+f = open('images.txt', 'w')
+for item in items:
+    img = item.find('img')['src']
+    if img.find('noimage') == -1:
+        f.write("https:" + img + "\n")
+
+f.close
+
